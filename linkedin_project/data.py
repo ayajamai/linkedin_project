050bb3913a00f8cbb3ad106313358fd0602feb68
Bruno Velasquez <velasquez.bruno@gmail.com>
2021-12-02 15:40:25 -0300

    changed number of rows in data.py

--- linkedin_project/data.py
+++ linkedin_project/data.py
@@ -3,15 +3,13 @@ from linkedin_project.params import BUCKET_NAME, BUCKET_TRAIN_DATA_PATH
 
 import pandas as pd
 
-def get_data_from_gcp(nrows=10000, optimize=False, **kwargs):
+def get_data_from_gcp(nrows=100, optimize=False, **kwargs):
     """method to get the training data (or a portion of it) from google cloud bucket"""
     # Add Client() here
     client = storage.Client()
     path = f"gs://{BUCKET_NAME}/{BUCKET_TRAIN_DATA_PATH}"
     df = pd.read_csv(path, nrows=nrows)
     return df
-
-# TO DOOOO
 
 if __name__ == '__main__':
     df = get_data_from_gcp()
